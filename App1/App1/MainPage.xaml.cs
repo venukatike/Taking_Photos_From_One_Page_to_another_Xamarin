b7c28e4c3400e2ceff9e58f9dcb9b95754b7247c
Venu <Venu@DESKTOP-PADD8DL>
2023-04-27 01:24:08 +0530

    Add project files.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Plugin.Media.Abstractions;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public static FileResult CapturedPhoto { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void btn1(object sender, EventArgs e)
+        {
+            var photo = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+            {
+                Title="Take a Photo"
+            });
+                CapturedPhoto = photo;
+            }
+        async void btn2(object sender, EventArgs e)
+        {
+            var photo = await MediaPicker.CapturePhotoAsync();
+            CapturedPhoto = photo;
+        }
+
+        async void btn3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+       
+    }
+}
---
+++ App1/App1/Page1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+
+        public Page1()
+        {
+            var ph = MainPage.CapturedPhoto;
+            InitializeComponent();
+            if (ph != null)
+            {
+                MyImage.Source = ImageSource.FromFile(ph.FullPath);
+            }
+        }
+
+    }
+}
